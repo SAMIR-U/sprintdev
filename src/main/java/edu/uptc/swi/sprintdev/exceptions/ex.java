8b1f97fc72c97e57bc357ede2c2d0a9e73acd500
Mateo-Castro06 <mateoruedacasdtro11@gmail.com>
2026-07-06 16:42:25 -0500

    implementacion usuario service

--- src/main/java/edu/uptc/swi/sprintdev/exceptions/ex.java
+++
@@ -1,7 +0,0 @@
-package edu.uptc.swi.sprintdev.exceptions;
-
-public class ex extends RuntimeException {
-    public ex(String message) {
-        super(message);
-    }
-}
--- src/main/java/edu/uptc/swi/sprintdev/service/UserServiceImpl.java
+++ src/main/java/edu/uptc/swi/sprintdev/service/UserServiceImpl.java
@@ -1,4 +1,42 @@
 package edu.uptc.swi.sprintdev.service;
 
-public class UserServiceImpl {
+import edu.uptc.swi.sprintdev.domain.User;
+import edu.uptc.swi.sprintdev.exceptions.UserNameAlreadyExist;
+import edu.uptc.swi.sprintdev.repository.IUserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserServiceImpl implements IUserService {
+    @Autowired
+    private IUserRepo userRepo;
+
+    @Override
+    public boolean registerUser(User user) {
+        if (!this.userExist(user)) {
+            this.userRepo.save(user);
+            return true;
+        }
+        throw new UserNameAlreadyExist(user.getFullName());
+    }
+
+    @Override
+    public boolean loginUser(String userName, String password) {
+        User tempUser = this.userRepo.findByUsername(userName);
+        if (tempUser != null) {
+            return this.isCorrectPassword(tempUser, password);
+        }
+        return false;
+    }
+
+    @Override
+    public User obtainUserByUsername(String username) {
+        return this.userRepo.findByUsername(username);
+    }
+
+    private boolean userExist(User user) {
+        return this.userRepo.findByUsername(user.getFullName()) != null;
+    }
+
+    private boolean isCorrectPassword(User user, String password) {
+        return user.getPasswordHash().equals(password);
+    }
 }
