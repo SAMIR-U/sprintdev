9d363655859d088e7dce27751d4d2af48f84abba
SAMIR-U <samir.borda@uptc.edu.co>
2026-07-06 18:43:21 -0500

    Login controller created

--- src/main/java/edu/uptc/swi/sprintdev/controller/LoginController.java
+++ src/main/java/edu/uptc/swi/sprintdev/controller/LoginController.java
@@ -2,11 +2,36 @@ package edu.uptc.swi.sprintdev.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import edu.uptc.swi.sprintdev.service.IUserService;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
+
+    private final IUserService userService;
+
+    public LoginController(IUserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/")
     public String loginPage() {
+        return "index";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String user,
+                        @RequestParam String password,
+                        HttpSession session) {
+
+        if (userService.loginUser(user, password)) {
+            session.setAttribute("loginmessage", "success");
+            return "mainmenu";
+        }
+        session.setAttribute("loginmessage", "fail");
         return "index";
     }
 }
