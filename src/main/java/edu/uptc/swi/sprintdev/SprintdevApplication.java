dc3eb079125445e3985b509a7a40f59107568df3
SAMIR-U <samir.borda@uptc.edu.co>
2026-07-06 19:32:18 -0500

    RegistUserController added

--- src/main/java/edu/uptc/swi/sprintdev/SprintdevApplication.java
+++ src/main/java/edu/uptc/swi/sprintdev/SprintdevApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SprintdevApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SprintdevApplication.class, args);
 	}
---
+++ src/main/java/edu/uptc/swi/sprintdev/controller/RegistUserController.java
@@ -0,0 +1,41 @@
+package edu.uptc.swi.sprintdev.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import edu.uptc.swi.sprintdev.domain.User;
+import edu.uptc.swi.sprintdev.service.IUserService;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class RegistUserController {
+    private final IUserService userService;
+
+    public RegistUserController(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/registuser")
+    public String loginPage() {
+        return "registuser";
+    }
+
+    @PostMapping("/registuser")
+    public String login(@RequestParam String user,
+                        @RequestParam String password,
+                        HttpSession session) {
+
+        User userObj = new User();
+        userObj.setUserName(user);
+        userObj.setPassword(password);
+
+        if (userService.registerUser(userObj)) {
+            session.setAttribute("registmessage", "success");
+            return "mainmenu";
+        }
+        session.setAttribute("registmessage", "fail");
+        return "registuser";
+    }
+}
