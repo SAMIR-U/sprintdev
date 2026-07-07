b1fbe11454a5e94b076ab0be99e9df4811de48ca
SAMIR-U <samir.borda@uptc.edu.co>
2026-07-06 22:14:08 -0500

    LogoutController added

---
+++ src/main/java/edu/uptc/swi/sprintdev/controller/LogoutController.java
@@ -0,0 +1,17 @@
+package edu.uptc.swi.sprintdev.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class LogoutController {
+    
+    @PostMapping("/logout")
+    public String logout(HttpSession session) {
+        session.removeAttribute("user");
+        session.removeAttribute("registmessage");
+        return "redirect:/index";
+    }
+}
