a5034a5e4e4581d71a4e2112888a975812e8af4c
SAMIR-U <samir.borda@uptc.edu.co>
2026-07-08 20:29:28 -0500

    import correction un homecontroller

--- src/main/java/edu/uptc/swi/sprintdev/controller/HomeController.java
+++ src/main/java/edu/uptc/swi/sprintdev/controller/HomeController.java
@@ -2,13 +2,12 @@ package edu.uptc.swi.sprintdev.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String getMethodName(@RequestParam String param) {
+    public String getMethodName() {
         return "index";
     }
     
