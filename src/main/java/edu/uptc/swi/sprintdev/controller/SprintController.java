333dd94217cac76dc5a92c8c05c73e8ab208af38
SAMIR-U <samir.borda@uptc.edu.co>
2026-07-09 18:43:30 -0500

    SprintController accestToSprint implemented

--- src/main/java/edu/uptc/swi/sprintdev/controller/SprintController.java
+++ src/main/java/edu/uptc/swi/sprintdev/controller/SprintController.java
@@ -77,9 +77,11 @@ public class SprintController {
 
         Sprint sprint = sprintService.findSprintById(sprintId, user.getId());
         List<User> readers = sprintService.findAllReadersSprint(sprintId, user.getId());
+        User creator = sprintService.obtainCreator(sprintId);
 
         session.setAttribute("sprint", sprint);
         session.setAttribute("readers", readers);
+        session.setAttribute("creator", creator);
 
         return "sprint";
     }
