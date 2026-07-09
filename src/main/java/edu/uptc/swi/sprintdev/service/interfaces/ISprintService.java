c4a87a41787daa2425adf10008e0eeee2ceaaaee
Mateo-Castro06 <mateoruedacastro11@gmail.com>
2026-07-08 22:40:48 -0500

    implementación excepcion en la interfaz service

--- src/main/java/edu/uptc/swi/sprintdev/service/interfaces/ISprintService.java
+++ src/main/java/edu/uptc/swi/sprintdev/service/interfaces/ISprintService.java
@@ -2,15 +2,16 @@ package edu.uptc.swi.sprintdev.service.interfaces;
 
 import edu.uptc.swi.sprintdev.domain.Sprint;
 import edu.uptc.swi.sprintdev.domain.User;
+import edu.uptc.swi.sprintdev.exceptions.UserDontHavePermissionException;
 
 import java.util.List;
 
 public interface ISprintService {
     boolean createSprint(Sprint sprint);
     List<Sprint> obtainMySprints(int userId);
-    List<User> findAllReadersSprint(int sprintId, int userId);
-    boolean closeSprint(int sprintId, int creatorId);
-    boolean activateSprint(int sprintId, int creatorId);
-    boolean addReaderToSprint(int sprintId,int creatorId ,User user);
-    Sprint findSprintById(int sprintId, int userId);
+    List<User> findAllReadersSprint(int sprintId, int userId) throws UserDontHavePermissionException;
+    boolean closeSprint(int sprintId, int creatorId) throws UserDontHavePermissionException;
+    boolean activateSprint(int sprintId, int creatorId) throws UserDontHavePermissionException;
+    boolean addReaderToSprint(int sprintId,int creatorId ,User user) throws UserDontHavePermissionException;
+    Sprint findSprintById(int sprintId, int userId) throws UserDontHavePermissionException;
 }
--- src/main/java/edu/uptc/swi/sprintdev/service/interfaces/ISprintTaskService.java
+++ src/main/java/edu/uptc/swi/sprintdev/service/interfaces/ISprintTaskService.java
@@ -1,10 +1,11 @@
 package edu.uptc.swi.sprintdev.service.interfaces;
 
 import edu.uptc.swi.sprintdev.domain.Task;
+import edu.uptc.swi.sprintdev.exceptions.UserDontHavePermissionException;
 
 public interface ISprintTaskService {
-    boolean createTask(Task task, int creatorId);
-    boolean updateTask(Task task, int creatorId);
-    boolean deleteTask(Task task, int creatorId);
-    Task findTaskById(int id);
+    boolean createTask(Task task, int creatorId) throws UserDontHavePermissionException;
+    boolean updateTask(Task task, int creatorId) throws UserDontHavePermissionException;
+    boolean deleteTask(Task task, int creatorId) throws UserDontHavePermissionException;
+    Task findTaskById(int id) throws UserDontHavePermissionException;
 }
