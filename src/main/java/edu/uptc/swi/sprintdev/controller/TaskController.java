43ec34d02422b17b49bcbfa0f3d66d00332df2c3
SAMIR-U <samir.borda@uptc.edu.co>
2026-07-09 20:16:52 -0500

    Not used imports removed in TaskController

--- src/main/java/edu/uptc/swi/sprintdev/controller/TaskController.java
+++ src/main/java/edu/uptc/swi/sprintdev/controller/TaskController.java
@@ -1,14 +1,11 @@
 package edu.uptc.swi.sprintdev.controller;
 
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import edu.uptc.swi.sprintdev.controller.utils.SessionUtlis;
 import edu.uptc.swi.sprintdev.domain.Sprint;
