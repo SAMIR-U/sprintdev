ceac473620763c5b35f7609da72bdec72e2f71c1
Didier-molina <Didier.molina@uptc.edu.co>
2026-07-13 21:37:17 -0500

    Add documentation for exception classes

--- src/main/java/edu/uptc/swi/sprintdev/exceptions/SprintIsNotActiveException.java
+++ src/main/java/edu/uptc/swi/sprintdev/exceptions/SprintIsNotActiveException.java
@@ -1,5 +1,8 @@
 package edu.uptc.swi.sprintdev.exceptions;
 
+/**
+ * Exception thrown when an operation requires an active sprint but the sprint is not active.
+ */
 public class SprintIsNotActiveException extends RuntimeException {
     public SprintIsNotActiveException(String message) {
         super(message);
