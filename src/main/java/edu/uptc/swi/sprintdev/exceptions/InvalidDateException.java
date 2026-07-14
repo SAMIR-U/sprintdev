66c3a402b77450e508512d00140b3282181e711b
Didier-molina <Didier.molina@uptc.edu.co>
2026-07-13 21:35:39 -0500

    Add documentation for InvalidDateException

--- src/main/java/edu/uptc/swi/sprintdev/exceptions/InvalidDateException.java
+++ src/main/java/edu/uptc/swi/sprintdev/exceptions/InvalidDateException.java
@@ -1,5 +1,9 @@
 package edu.uptc.swi.sprintdev.exceptions;
 
+/**
+ * Exception thrown when a sprint date input is invalid.
+ * Examples include end dates before start dates or malformed date values.
+ */
 public class InvalidDateException extends RuntimeException {
     public InvalidDateException(String message) {
         super(message);
